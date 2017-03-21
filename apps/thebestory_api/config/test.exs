c74fd9ac850a6213f5401a79602b816c049cd206
Nariman Safiulin <woofilee@gmail.com>
2017-03-21 16:15:29 +0300

    We can start only one server on port per one task (API or Web part), thats why we can use the same port in the configurations. (Not really, we can start two in the parallel on different ports, but on Heroku, only one port per application, and, in the future, we will start this parts on different servers, each only for API or Web)

--- apps/thebestory_api/config/test.exs
+++ apps/thebestory_api/config/test.exs
@@ -3,5 +3,5 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :thebestory_api, TheBestory.API.Endpoint,
-  http: [port: 4444],
+  http: [port: 4443],
   server: false
