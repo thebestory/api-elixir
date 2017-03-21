94de777dea4930f548655c1269f9a3cd23f6b5cb
Nariman Safiulin <woofilee@gmail.com>
2017-03-21 16:44:52 +0300

    Settings for easy starting parts of app

--- apps/thebestory_api/lib/thebestory_api/application.ex
+++ apps/thebestory_api/lib/thebestory_api/application.ex
@@ -4,17 +4,25 @@ defmodule TheBestory.API.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Start the endpoint when the application starts
-      supervisor(TheBestory.API.Endpoint, []),
-      # Start your own worker by calling: TheBestory.API.Worker.start_link(arg1, arg2, arg3)
-      # worker(TheBestory.API.Worker, [arg1, arg2, arg3]),
-    ]
-
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: TheBestory.API.Supervisor]
-    Supervisor.start_link(children, opts)
+
+    case System.get_env("SERVER_TYPE") do
+      "api" -> 
+        # Define workers and child supervisors to be supervised
+        children = [
+          # Start the endpoint when the application starts
+          supervisor(TheBestory.API.Endpoint, []),
+          # Start your own worker by calling: TheBestory.API.Worker.start_link(arg1, arg2, arg3)
+          # worker(TheBestory.API.Worker, [arg1, arg2, arg3]),
+        ]
+
+        Supervisor.start_link(children, opts)
+      "web" ->
+        Supervisor.start_link([], opts)
+      _ ->
+        Supervisor.start_link([], opts)
+    end
   end
 end
--- apps/thebestory_web/lib/thebestory_web/application.ex
+++ apps/thebestory_web/lib/thebestory_web/application.ex
@@ -4,17 +4,25 @@ defmodule TheBestory.Web.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Start the endpoint when the application starts
-      supervisor(TheBestory.Web.Endpoint, []),
-      # Start your own worker by calling: TheBestory.Web.Worker.start_link(arg1, arg2, arg3)
-      # worker(TheBestory.Web.Worker, [arg1, arg2, arg3]),
-    ]
-
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: TheBestory.Web.Supervisor]
-    Supervisor.start_link(children, opts)
+
+    case System.get_env("SERVER_TYPE") do
+      "api" -> 
+        Supervisor.start_link([], opts)
+      "web" ->
+        # Define workers and child supervisors to be supervised
+        children = [
+          # Start the endpoint when the application starts
+          supervisor(TheBestory.Web.Endpoint, []),
+          # Start your own worker by calling: TheBestory.Web.Worker.start_link(arg1, arg2, arg3)
+          # worker(TheBestory.Web.Worker, [arg1, arg2, arg3]),
+        ]
+
+        Supervisor.start_link(children, opts)
+      _ -> 
+        Supervisor.start_link([], opts)
+    end
   end
 end
