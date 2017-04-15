f4177573ccc07855dbc5263cfd947874324189c9
Nariman Safiulin <woofilee@gmail.com>
2017-04-16 01:26:09 +0300

    Order of tests changed

--- apps/thebestory/test/stores/id_test.exs
+++ apps/thebestory/test/stores/id_test.exs
@@ -6,6 +6,16 @@ defmodule TheBestory.Stores.IDTest do
   @id_type "test"
   @default_id_type "id"
 
+  test "get/1 returns the ID object by the given id" do
+    {:ok, id} = Stores.ID.generate()
+    assert Stores.ID.get(id.id) == id
+  end
+
+  test "get!/1 returns the ID object by the given id" do
+    {:ok, id} = Stores.ID.generate()
+    assert Stores.ID.get!(id.id) == id
+  end
+
   test "generate/1 with id type generates an ID" do
     {:ok, id} = Stores.ID.generate(@id_type)
 
@@ -22,16 +32,6 @@ defmodule TheBestory.Stores.IDTest do
 
   test "generate/1 with invalid type returns error" do
     assert {:error, :id_not_generated} = Stores.ID.generate(nil)
-  end
-
-  test "get/1 returns the ID object by the given id" do
-    {:ok, id} = Stores.ID.generate()
-    assert Stores.ID.get(id.id) == id
-  end
-
-  test "get!/1 returns the ID object by the given id" do
-    {:ok, id} = Stores.ID.generate()
-    assert Stores.ID.get!(id.id) == id
   end
 
   test "update/2 with a new type updates the ID" do
